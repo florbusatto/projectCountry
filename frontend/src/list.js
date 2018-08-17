54f90d0b2292a1eebaf181c151c9b1d01257c1d3
[florencia] <[florencia_busatto@hotmail.com]>
2018-08-17 01:49:51 -0300

    cambiosfinales

---
+++ frontend/src/list.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './list.css';
+
+class list extends Component {
+
+ render() {
+
+    return (
+       <div className="List">
+        
+      </div>
+    );
+  }
+}
+
+      
+export default list;
