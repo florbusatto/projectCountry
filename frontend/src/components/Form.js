f677162dac3dbc481b1f23ab32029f47b354fd0c
[florencia] <[florencia_busatto@hotmail.com]>
2018-08-23 12:51:00 -0300

    final_version

--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import '../css/Form.css';
+import '../css/form.css';
 import {List} from './List'
 import {Hello} from './Hello'
 
 class form extends Component {
+
+//1st:Create the constructor and set the states of your component//
+//a)countriesSelect array: Countries list built from Api:https://restcountries.eu/rest/v2/all//
+//b)Name & Surname: Field that visitor fills with her/his name and lastname.//
+//c)Country: The visitor selects a country from the array.
+//d)Birthday: Field that visitor fills with her/his birth date.
 
 constructor (props) {
    super(props);
@@ -19,6 +25,11 @@ constructor (props) {
 
 }
 
+//2nd: Lifecycle method "DidMount" - API Calling//
+//a)I used "fetch" in order to call the information from the API.//
+//b)After that, data shows each country name from the list.//
+//If you want to know that this worked you can check it at your browser's console.//
+
 componentDidMount() {
 fetch('https://restcountries.eu/rest/v2/all')
 .then(results => {
@@ -31,42 +42,55 @@ fetch('https://restcountries.eu/rest/v2/all')
 })
 }
 
+
+//3rd: Create the onSubmit event & New Item//
+//a)onSubmit event: This event will be invoked when the visitor submits her/his previous data//
+//b)Let newReg: It refers to the creation of a new item//
+
 onSubmit =(e) => {
  e.preventDefault();
  console.log(this.state);
 
- let newReg={
+let newReg={
   name:this.state.Name,
   surname:this.state.Surname,
   country:this.state.Country,
-  birthday:this.state.Birthday
+  birthday:this.state.Birthday,
  }
 
  this.setState({sent:true})
 
 
-//******LOCALSTORAGE********//
+               //******LOCALSTORAGE********//
+
+//4th: Using localStorage: I used this method as is the most recommended when the data needs to be persistent //
 
         var regs = window.localStorage.getItem('items');
 
         if (regs === null) {
-            regs = []; 
+            regs = []; //Creating an empty list//
         } else {
             regs = JSON.parse(regs);
         }
-        regs.push(newReg);
+        regs.push(newReg);//Pushing new register at the end of the list//
 
-        this.setState({ List: regs})
+        this.setState({ List: regs})//Updating list in order to send data to List component//
          let items = JSON.stringify(regs);
 
-        window.localStorage.setItem('items', items);
+        window.localStorage.setItem('items', items);//Saving at localStorage//
         
 
-////////Limpieza de localStorage////////
-localStorage.clear();
+//Clean localStorage//
+//If you don't want to see the complete list of recent visitors,//
+//please uncomment the sentence at line 87//
+
+//localStorage.clear();
 //*************************************//
 
 };
+
+//5th: onClear Event: This event will be invoked 
+//when a new visitor wants to submit her/his new register//
 
 onClear=(e)=>{
   e.preventDefault();
@@ -76,49 +100,61 @@ onClear=(e)=>{
   this.setState({sent:false});
 }
 
+//6th: Time to show up!: Render.
+//I choose to show the form data as a table.
+//When the main Div closes, it's important to remember//
+//to close the component with the export line at the end//
+
 render() {
    return (
     <div className="main">
      <form>
        <table>
-        <tr>
-         <td>
-          <label>Name:</label>
-          <input placeholder="name here" value={this.state.Name} onChange={e =>this.setState({ Name: e.target.value})}/>
-         </td>
+         <tr>
+          <td>
+           <label>Name:</label>
+           <input placeholder=" name here " value={this.state.Name} onChange={e =>this.setState({ Name: e.target.value})}/>
+          </td>
+         </tr>
+         <tr>
+          <td>
+           <label>Surname:</label>
+           <input placeholder= " name here " value={this.state.Surname} onChange={e =>this.setState({ Surname: e.target.value})}/>
+          </td>
+         </tr>
+         <tr> 
+          <td>
+           <label>Countries:</label>
+           <select value={this.state.Country} onChange={e =>this.setState({ Country: e.target.value})}>
+            <option value=''>Countries</option>
+            {this.state.countriesSelect.map(country=><option value={country}>{country}</option>)}
+           </select>
+          </td>
+         </tr>
+         <tr>
+          <td>
+           <label>Birthday:</label>
+           <input type="date" name=" fecha " value={this.state.Birthday} onChange={e =>this.setState({ Birthday: e.target.value})}/>
+           <button onClick={(e) => this.onSubmit(e)}>Save</button>
+           <button onClick={(e) => this.onClear(e)}>New Register</button>  
+          </td> 
         </tr>
-        <tr>
-         <td>
-          <label>Surname:</label>
-          <input placeholder= "name here" value={this.state.Surname} onChange={e =>this.setState({ Surname: e.target.value})}/>
-         </td>
-        </tr>
-        <tr> 
-         <td>
-          <label>Countries:</label>
-          <select value={this.state.Country} onChange={e =>this.setState({ Country: e.target.value})}>
-           <option value=''>Choose an option</option>
-           {this.state.countriesSelect.map(country=><option value={country}>{country}</option>)}
-          </select>
-         </td>
-        </tr>
-        <tr>
-          <td><label>Birthday:</label>
-          <input type="date" name="fecha" value={this.state.Birthday} onChange={e =>this.setState({ Birthday: e.target.value})}/>
-        </td>
-        </tr>
-         <button onClick={(e) => this.onSubmit(e)}>Save</button>
-         <button onClick={(e) => this.onClear(e)}>New Registration</button>
-         <p>{this.state.sent?<Hello name={this.state.Name} from country={this.state.Country} surname={this.state.Surname} birthday={this.state.Birthday}/>:''} </p>
-       </table>
-       </form>
-
-      <List list={this.state.List}/>
+      </table>  
+    </form>
       
-    </div>
-   );
+      <p>{this.state.sent?<Hello name={this.state.Name} from country={this.state.Country} surname={this.state.Surname} birthday={this.state.Birthday}/>:''} </p>
+      
+      <List list={this.state.List}/>
+    
+    <footer>
+     <h2>Florencia Busatto</h2>
+    </footer>
+</div>
+   
+    );
   
- }
+  }
+
 }
 
 export default form;
--- frontend/src/components/Hello.js
+++ frontend/src/components/Hello.js
@@ -1,11 +1,47 @@
 import React from 'react';
 import '../css/hello.css';
 
+//General information: 
+//This representational component was created in order to show a 
+//greeting message to every visitor that submits her/his data//
+
+
+//1st: I created a function to calculate visitor's age //
+
+function calculateAge (date) {
+  var values=date.split("-");
+  var day = values[2];
+  var month = values[1];
+  var year = values[0];
+
+  var current_date = new Date();
+  var current_year= current_date.getFullYear();
+  var current_month = current_date.getMonth()+1;
+  var current_day = current_date.getDate();
+
+  var age = current_year - year + 1;
+  if ( current_month < month )
+  {
+      age--;
+  }
+  if ((month == current_month) && (current_day < day))
+  {
+      age--;
+  }
+
+  return age;
+}
+
+//2nd: I created the following "const" to show  
+//the day,the month and the age of the visitor in the message//
+//Line 46 contains greeting's structure//
+
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const getDay = date => date.split("-")[2]
+const getMonth = date => months[parseInt(date.split("-")[1]) - 1 ]
+
 export const Hello = (props) =>
 
 <div className="Hello">
-      <p>Hello<h5>{props.name+' '}{props.surname+' '} from {props.country+' '}</h5></p>
+ <p>Hello {props.name+' '}{props.surname+' '} from {props.country+' '} on {getDay(props.birthday)} of {getMonth(props.birthday)+ '' } you will have {calculateAge(props.birthday) + ' years '}</p>
 </div>
-
-
-
